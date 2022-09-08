bc61385eb20a5cee9f3c0e3781ff91e6174d9056
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-08 20:46:54 +0600

    Create prob-2.cpp

---
+++ prob-2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n,i,max_count=0,count=0;
+    cin>>n;
+    char line[n];
+    for (i=0;i<n;i++){
+     cin>>line[i];
+    }
+    char first = line[0];
+    for (i=0;i<n;i++){
+        if (first==line[i]){
+            count++;
+        }
+        else {
+            first = line[i];
+            count = 0;
+        }
+        if (count>max_count){
+            max_count = count;
+        }
+    }
+    cout<<max_count<<endl;
+    return 0;
+}
