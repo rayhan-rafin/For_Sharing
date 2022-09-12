cc557c71f6e293326e85133547c94a458b5f46c8
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-12 19:38:37 +0600

    Update and rename graph_mod to BE_graph_mod.cpp

--- BE_graph_mod.cpp
+++ BE_graph_mod.cpp
@@ -5,14 +5,20 @@ int main()
 {
     int t;
     float v[15];
+    float id[15];
     cout<<"input vd :"<<endl;
     for (int i=0 ; i<15 ;i++)
     {
         cin>>v[i];
     }
+    cout<<"input Id :"<<endl;
+    for (int i=0 ; i<15 ;i++)
+    {
+        cin>>id[i];
+    }
     for (int i=0; i<15;  i++)
     {
-       cout<<i+1<<"    =    "<<v[i]*2<<endl; // change miltiplier(2) according to your need
+       cout<<i+1<<"    =    "<<v[i]*5 <<" - "<<id[i]*4<<endl; // change miltiplier(2) according to your need
     }
     return 0;
 }
